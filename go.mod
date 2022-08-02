47a059751b323df5a525f0c6c909343ce23f008f
seekwe <soseek@vip.qq.com>
2022-08-02 15:24:30 +0800

    ⚙️ refactor: call method change

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module sse
 
 go 1.18
+
+require github.com/sohaha/zlsgo v1.4.2
+
+require (
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+)
+
+replace github.com/sohaha/zlsgo => ../zlsgo-dev
--- serve/main.go
+++ serve/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sohaha/zlsgo/zlog"
 	"github.com/sohaha/zlsgo/znet"
 	"github.com/sohaha/zlsgo/znet/cors"
+	"github.com/sohaha/zlsgo/zpprof"
 	"github.com/sohaha/zlsgo/ztime"
 	"github.com/sohaha/zlsgo/ztype"
 )
@@ -17,6 +18,9 @@ func main() {
 
 	// 设置为开发模式
 	r.SetMode(znet.DebugMode)
+
+	// 开启 pprof
+	zpprof.Register(r, "")
 
 	r.Log.ResetFlags(zlog.BitLevel | zlog.BitTime)
 
@@ -32,44 +36,45 @@ func main() {
 	// 注册路由
 	r.GET("/sse", func(c *znet.Context) {
 		noretry := c.DefaultQuery("noretry", "")
+		id := 0
 		s := znet.NewSSE(c, func(lastID string, opts *znet.SSEOption) {
-			opts.RetryTime = 5000
+			opts.RetryTime = 1000
 			if lastID != "" {
-				zlog.Tips("This is even the request again:", lastID, noretry)
-				if noretry != "" {
-					opts.Verify = false
-				}
+				id = ztype.ToInt(lastID)
+				c.Log.Tips("This is even the request again:", lastID, noretry)
 			}
 		})
 
-		id := 0
+		go func() {
+			i := 1
+			_ = s.Send(ztype.ToString(id+i), "Hi 😋", "system")
 
-		s.Send(ztype.ToString(id), "Hi 😋", "system")
-
-		for {
-			select {
-			case <-s.Done():
-				zlog.Warn("disconnects")
-				return
-			default:
-				id++
-				// 定时发送当前时间
-				now := ztime.Now()
-				s.Send(ztype.ToString(id), now)
-
-				time.Sleep(time.Second * 1)
-
-				// 5 次之后取消
-				if id >= 5 {
-					s.Send("-1", "This is the last one", "system")
+			for {
+				select {
+				case <-s.Done():
 					return
+				default:
+					i++
+
+					// 定时发送当前时间
+					now := ztime.Now()
+					_ = s.Send(ztype.ToString(id+i), now)
+
+					// N 次之后取消
+					if i >= 5 {
+						s.Stop()
+					}
+
+					time.Sleep(time.Second * 1)
 				}
 			}
-		}
+		}()
+
+		s.Push()
 	})
 
 	r.GET("/", func(c *znet.Context) {
-		c.String(200, "Hello world")
+		c.HTML(200, `<html><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>SSE</title><body><ul id="app"></ul><script>var source = new EventSource("/sse?key=123"); source.addEventListener("message",function(event) {var li=document.createElement("li");li.innerText=event.data;document.querySelector("#app").appendChild(li);console.log("message",event.data)});source.addEventListener("open",function(event) {console.log("连接已经建立",event)});source.addEventListener("error",function(event,e) {console.log("error",event,e)});</script></body></html>`)
 	})
 
 	// 启动
